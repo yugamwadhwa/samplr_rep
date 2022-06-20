6f453be93698294310d2454d84fb501a27e00445
yugamwadhwa <yugamwadhwa1234@gmail.com>
2022-06-20 19:04:42 +0530

    new

---
+++ test copy.php
@@ -0,0 +1,16 @@
+<?php
+echo "<table border=1>
+      <tr>
+      <td>Salary of Mr A</td>
+      <td>1000$</td>
+      </tr>
+      <tr>
+      <td>Salary of Mr B</td>
+      <td>1200$</td>
+      </tr>
+      <tr>
+      <td>Salary of Mr A</td>
+      <td>1400$</td>
+      </tr>
+      </table>"
+?>
